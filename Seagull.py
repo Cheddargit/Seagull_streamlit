9001ff179bb95834575a0db8ffa6fa4f1acbfae8
Cheddar Developer <113924487+Cheddargit@users.noreply.github.com>
2022-09-19 17:15:20 +0100

    Update Seagull.py
    added 4 menu items

--- Seagull.py
+++ Seagull.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinter')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocekt Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
