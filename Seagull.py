1f8fa98c0d320cd9d70a517e922621a6e6bc3fa9
Cheddar Developer <113924487+Cheddargit@users.noreply.github.com>
2022-09-22 16:57:08 +0100

    Update Seagull.py
    add a function

--- Seagull.py
+++ Seagull.py
@@ -17,6 +17,12 @@ fruits_selected =streamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+#create the repeatable block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    # use pandas to format the jason output that comes from fruityvice
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -24,11 +30,9 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-    # use pandas to format the jason output that comes from fruityvice
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function=get_fruityvice_data(fruit_choice)
     # display normalized data
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 # don't run anything past here while we troubleshoot
