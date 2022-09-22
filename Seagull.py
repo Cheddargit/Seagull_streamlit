1791a5dea5b4914cb295d47271eeb1b0aa2f5672
Cheddar Developer <113924487+Cheddargit@users.noreply.github.com>
2022-09-22 14:13:46 +0100

    Update Seagull.py
    Prettified output from fruityvice, using pandas

--- Seagull.py
+++ Seagull.py
@@ -18,4 +18,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# use pandas to format the jason output that comes from fruityvice
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display normalized data
+streamlit.dataframe(fruityvice_normalized)
